c630b70cb3d3d4e8b9f4ecd4343be8436b2418c8
Luiz Eduardo <luizdudumr@gmail.com>
2021-08-04 20:51:17 -0300

    first files added

---
+++ Zipzap/Zipzap/ZipzapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipzapApp.swift
+//  Zipzap
+//
+//  Created by Luiz Eduardo Mello dos Reis on 04/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ZipzapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
